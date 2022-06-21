dabd6c5028d9e2517a7d2e071a21e0706ca26283
Armen-ar <armen.aruts1967@gmail.com>
2022-06-21 14:54:33 +0300

    with_an_error_3

--- part1/views_division_2/models.py
+++ part1/views_division_2/models.py
@@ -3,6 +3,7 @@ from setup_db import db
 
 class Book(db.Model):
     __tablename__ = 'book'
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     author = db.Column(db.String(100))
